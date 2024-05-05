504bcb9f6d14148b157e308a7ffe4b6898c35c0d
Agnieszka <agamjp@gmail.com>
2024-05-05 22:14:27 +0200

    added my picture, applied column layout, created description

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/apw_2024.jpg")
+
+with col2:
+    st.title("Agnieszka Przybyła-Wilkin")
+    with open("about.txt", 'r', encoding="utf8") as file:
+        content = file.read()
+    st.info(content)
