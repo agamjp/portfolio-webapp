6d058cf2d48fb115e7015a9e595bff4f4b8562b7
Agnieszka <agamjp@gmail.com>
2024-05-07 13:47:06 +0200

    sending emails using the form
    (cherry picked from commit 54e86961e09b3c2b83452ba4c43d77de5f4c50a2)

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,3 +1,28 @@
 import streamlit as st
+from send_email import send_email
+import re
 
 st.header("Contact me")
+
+with st.form("contact"):
+    user_email = st.text_input(label="Enter your email address:", key="user_email", )
+    label = st.markdown("")
+    raw_message = st.text_area(label="Enter your message:", key="message")
+    button = st.form_submit_button("Submit")
+    if button:
+        message = f"""\
+Subject: Portfolio: New message from {user_email}
+
+User {user_email} sent the following message:
+
+{raw_message}
+"""
+        message = message.encode()
+        pattern = re.compile("[A-Za-z0-9-.%+_]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}")
+        match = re.fullmatch(pattern, user_email)
+        print(match)
+        if match is not None:
+            send_email(message)
+            label = st.markdown(":green[Message sent!]")
+        else:
+            label = st.markdown(":red[Invalid email address!]")
